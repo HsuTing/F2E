c4e6ce1e217ba9e52a01ebbb26c5df388898d996
HsuTing <hsuting0106@gmail.com>
2021-11-04 21:39:35 +0800

    fix(f2e): fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   extends: 'next/core-web-vitals',
   rules: {
     'no-unused-vars': 'error',
+    '@next/next/no-html-link-for-pages': [
+      'error',
+      './packages/week-one/pages/',
+    ],
   },
 };
