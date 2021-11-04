2b5647121584e70ee45058b53607c2c9cbe3cf82
HsuTing <hsuting0106@gmail.com>
2021-11-04 23:03:01 +0800

    feat(pages): add module scss

--- packages/week-one/pages/index.tsx
+++ packages/week-one/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button } from 'antd';
 
-const Home = () => <Button>Home</Button>;
+import styles from './styles/index.module.scss';
+
+const Home = () => <Button className={styles.root}>Home</Button>;
 
 export default React.memo(Home);
