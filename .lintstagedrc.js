12788a3670c882df5f3fb302207169d3a3756424
HsuTing <hsuting0106@gmail.com>
2021-11-04 22:20:02 +0800

    feat(f2e): should ignore ts buildInfo and add json prettier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,4 +2,5 @@ module.exports = {
   '*.js': ['yarn eslint', 'prettier --write'],
   '*.scss': ['prettier --parser scss --write'],
   '**/package.json': ['prettier-package-json --write'],
+  '*.json': ['prettier --parser json --write'],
 };
