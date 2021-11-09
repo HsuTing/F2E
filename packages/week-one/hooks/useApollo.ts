8aa0e7c3d3282241638c1e4ee77d2a7929e9e3fd
HsuTing <hsuting0106@gmail.com>
2021-11-10 01:14:49 +0800

    feat(week-one): add error handler and retry

--- packages/week-one/hooks/useApollo.ts
+++ packages/week-one/hooks/useApollo.ts
@@ -1,17 +1,51 @@
 import { useMemo } from 'react';
 import type { NormalizedCacheObject } from '@apollo/client';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client';
+import { RetryLink } from '@apollo/client/link/retry';
+import { onError } from '@apollo/client/link/error';
 import { RestLink } from 'apollo-link-rest';
 import merge from 'deepmerge';
+import { notification } from 'antd';
 
 let apolloClientCache: ApolloClient<NormalizedCacheObject> | null = null;
 
 const createApolloClient = () =>
   new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: new RestLink({
-      uri: 'https://ptx.transportdata.tw/MOTC/v2/',
-    }),
+    link: ApolloLink.from([
+      new RetryLink({
+        attempts: {
+          retryIf: error => Boolean(error) && error.statusCode !== 401,
+        },
+      }),
+      onError(({ graphQLErrors, networkError }) => {
+        const errorLog =
+          typeof window === 'undefined'
+            ? console.error
+            : ({ name, ...message }: { name: string }) =>
+                notification.error({
+                  message: `${name} Error!`,
+                  description: JSON.stringify(message),
+                });
+
+        if (graphQLErrors)
+          graphQLErrors.forEach(graphQLError => {
+            errorLog({
+              ...graphQLError,
+              name: 'GraphQL',
+            });
+          });
+
+        if (networkError)
+          errorLog({
+            ...networkError,
+            name: 'Network',
+          });
+      }),
+      new RestLink({
+        uri: 'https://ptx.transportdata.tw/MOTC/v2/',
+      }),
+    ]),
     cache: new InMemoryCache(),
   });
 
