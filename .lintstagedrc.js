307ffbe136474a2f4f76721e31a2d1105f555060
HsuTing <hsuting0106@gmail.com>
2021-11-10 00:36:52 +0800

    feat(f2e): add graphql prettier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,7 @@ module.exports = {
   '*.js': ['yarn eslint', 'prettier --write'],
   '*.{ts,tsx}': ['yarn eslint', 'prettier --parser typescript --write'],
   '*.scss': ['prettier --parser scss --write'],
+  '*.graphql': ['prettier --parser graphql --write'],
   '**/package.json': ['prettier-package-json --write'],
   '*.json': ['prettier --parser json --write'],
 };
