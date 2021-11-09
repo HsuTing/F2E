83b8b083833dcc96cd030f77bc80aca691cf9938
HsuTing <hsuting0106@gmail.com>
2021-11-10 00:33:38 +0800

    feat(week-one): could auto generate typescript from schema

--- packages/week-one/pages/index.tsx
+++ packages/week-one/pages/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import { Button } from 'antd';
+import { gql, useQuery } from '@apollo/client';
 
+import type { getScenicSpot as getScenicSpotType } from '../gqls';
 import styles from './styles/index.module.scss';
 
-const Home = () => <Button className={styles.root}>Home</Button>;
+const getScenicSpot = gql`
+  query getScenicSpot {
+    scenicSpots
+      @rest(
+        type: "ScenicSpot"
+        path: "Tourism/ScenicSpot?$top=3&$format=JSON"
+      ) {
+      id: ID
+      name: Name
+    }
+  }
+`;
+
+const Home = () => {
+  const { data } = useQuery<getScenicSpotType>(getScenicSpot);
+
+  return (
+    <>
+      {data?.scenicSpots.map(({ id, name }) => (
+        <Button key={id} className={styles.root}>
+          {name}
+        </Button>
+      ))}
+    </>
+  );
+};
 
 export default React.memo(Home);
