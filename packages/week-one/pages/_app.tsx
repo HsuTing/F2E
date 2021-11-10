4b96a6a521fd642caa94e44fe5bc2bac2ec149cd
HsuTing <hsuting0106@gmail.com>
2021-11-11 01:17:15 +0800

    feat(week-one): add home page link

--- packages/week-one/pages/_app.tsx
+++ packages/week-one/pages/_app.tsx
@@ -2,13 +2,15 @@ import 'antd/dist/antd.css';
 import React from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Link from 'next/link';
 import { ApolloProvider } from '@apollo/client';
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
 
 import { useApollo } from '../hooks/useApollo';
 import styles from './styles/app.module.scss';
 
 const { Header, Content } = Layout;
+const { Item } = Menu;
 
 const App = ({ Component, pageProps }: AppProps) => {
   const client = useApollo(pageProps.initialApolloState);
@@ -23,7 +25,36 @@ const App = ({ Component, pageProps }: AppProps) => {
       </Head>
 
       <Layout className={styles.root}>
-        <Header className={styles.header}>test</Header>
+        <Header className={styles.header}>
+          <Link href="/">
+            <a className={styles.home}>愛歹丸</a>
+          </Link>
+
+          <div>
+            <Menu mode="horizontal">
+              {[
+                {
+                  text: '觀光景點',
+                  href: '/',
+                },
+                {
+                  text: '觀光旅宿',
+                  href: '/',
+                },
+                {
+                  text: '觀光活動',
+                  href: '/',
+                },
+              ].map(({ text, href }) => (
+                <Item key={text}>
+                  <Link href={href}>
+                    <a>{text}</a>
+                  </Link>
+                </Item>
+              ))}
+            </Menu>
+          </div>
+        </Header>
 
         <Content>
           <Component {...pageProps} />
