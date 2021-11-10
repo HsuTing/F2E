f57eee61c6e506e95389ebcd4e5a59bc00e0145f
HsuTing <hsuting0106@gmail.com>
2021-11-11 00:38:20 +0800

    feat(week-one): add default layout

--- packages/week-one/pages/_app.tsx
+++ packages/week-one/pages/_app.tsx
@@ -2,15 +2,25 @@ import 'antd/dist/antd.css';
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
+import { Layout } from 'antd';
 
 import { useApollo } from '../hooks/useApollo';
+import styles from './styles/app.module.scss';
+
+const { Header, Content } = Layout;
 
 const App = ({ Component, pageProps }: AppProps) => {
   const client = useApollo(pageProps.initialApolloState);
 
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <Layout>
+        <Header className={styles.header}>test</Header>
+
+        <Content>
+          <Component {...pageProps} />
+        </Content>
+      </Layout>
     </ApolloProvider>
   );
 };
