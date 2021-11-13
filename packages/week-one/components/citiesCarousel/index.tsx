c760f1e53fba4e72fcab3bbec22f880d531da431
HsuTing <hsuting0106@gmail.com>
2021-11-13 23:09:53 +0800

    feat(week-one): add cities locales

--- packages/week-one/components/citiesCarousel/index.tsx
+++ packages/week-one/components/citiesCarousel/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { useTranslation } from 'next-i18next';
-import { Typography } from 'antd';
+import { Typography, Carousel } from 'antd';
+
+import { CITIES } from './constants';
 
 const { Title } = Typography;
 
 const CitiesCarousel = () => {
   const { t } = useTranslation('home');
 
-  return <Title level={2}>{t('maybe-go')}</Title>;
+  return (
+    <>
+      <Title level={2}>{t('maybe-go')}</Title>
+
+      <Carousel>
+        {CITIES.map(city => {
+          <div key={city}>{t(city)}</div>;
+        })}
+      </Carousel>
+    </>
+  );
 };
 
 export default React.memo(CitiesCarousel);
