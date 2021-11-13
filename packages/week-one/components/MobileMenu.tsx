d181e1aeb1d8871bf437f661947b78da8e5145e8
HsuTing <hsuting0106@gmail.com>
2021-11-13 15:15:05 +0800

    feat(week-one): add mobile menu

--- packages/week-one/components/MobileMenu.tsx
+++ packages/week-one/components/MobileMenu.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useTranslation } from 'next-i18next';
 import { Button, Drawer } from 'antd';
+import { Menu } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
 
 interface PropsType {
@@ -7,8 +11,14 @@ interface PropsType {
   outOfBreakpoint: boolean;
 }
 
+const { Item, SubMenu, Divider } = Menu;
+
 const MobileMenu = ({ className, outOfBreakpoint }: PropsType) => {
+  const router = useRouter();
+  const { t, i18n } = useTranslation();
   const [visible, setVisible] = useState(false);
+  // @ts-ignore next-i18next types error
+  const locales = i18n.options.locales as string[];
 
   useEffect(() => {
     setVisible(false);
@@ -25,7 +35,38 @@ const MobileMenu = ({ className, outOfBreakpoint }: PropsType) => {
         onClose={() => setVisible(false)}
         placement="left"
       >
-        <div />
+        <Menu selectedKeys={[router.asPath]} mode="inline">
+          {['scenic-spots', 'hotels', 'activities'].map(key => (
+            <Item key={key}>
+              <Link href={`/${key}`}>
+                <a>{t(key)}</a>
+              </Link>
+            </Item>
+          ))}
+
+          <Divider />
+
+          {['locale', 'wish-list'].map(key =>
+            key === 'locale' ? (
+              <SubMenu key={key} title={t('locale.title')}>
+                {locales.map(locale => (
+                  <Item
+                    key={locale}
+                    onClick={() => i18n.changeLanguage(locale)}
+                  >
+                    {t(`locale.${locale}`)}
+                  </Item>
+                ))}
+              </SubMenu>
+            ) : (
+              <Item key={key}>
+                <Link href={`/${key}`}>
+                  <a>{t(key)}</a>
+                </Link>
+              </Item>
+            ),
+          )}
+        </Menu>
       </Drawer>
     </>
   );
