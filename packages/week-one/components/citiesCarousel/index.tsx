75f4ebfe23f2c33e3c3b2e1d4aff336aa0630bea
HsuTing <hsuting0106@gmail.com>
2021-11-13 23:40:35 +0800

    feat(week-one): add image width

--- packages/week-one/components/citiesCarousel/index.tsx
+++ packages/week-one/components/citiesCarousel/index.tsx
@@ -3,21 +3,34 @@ import { useTranslation } from 'next-i18next';
 import { Typography, Carousel } from 'antd';
 
 import { CITIES } from './constants';
+import { useImageWidth } from './hooks/useImageWidth';
 
 const { Title } = Typography;
 
 const CitiesCarousel = () => {
   const { t } = useTranslation('home');
+  const { wrapperRef, imageWidth } = useImageWidth();
 
   return (
     <>
       <Title level={2}>{t('maybe-go')}</Title>
 
-      <Carousel>
-        {CITIES.map(city => {
-          <div key={city}>{t(city)}</div>;
-        })}
-      </Carousel>
+      <div ref={wrapperRef}>
+        <Carousel variableWidth>
+          {CITIES.map(city => (
+            <div key={city}>
+              <div
+                style={{
+                  width: imageWidth,
+                  height: imageWidth,
+                }}
+              >
+                {t(`cities.${city}`)}
+              </div>
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </>
   );
 };
