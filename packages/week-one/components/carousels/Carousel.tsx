2f751be08ae32c5fe582040f69b1eea7974c3c49
HsuTing <hsuting0106@gmail.com>
2021-11-14 14:10:35 +0800

    feat(week-one): use antd carousel

--- packages/week-one/components/carousels/Carousel.tsx
+++ packages/week-one/components/carousels/Carousel.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useTranslation } from 'next-i18next';
-import { Typography, Button } from 'antd';
+import { Typography, Button, Carousel as AntdCarousel } from 'antd';
+
+import { useCarouselInfo } from '../../hooks/useCarouselInfo';
 
 import styles from './styles/carousel.module.scss';
 
@@ -12,12 +14,32 @@ const { Title } = Typography;
 
 const Carousel = ({ title }: PropsType) => {
   const { t } = useTranslation('carousels');
+  const { carouselRef, imageSize } = useCarouselInfo(350, '100%');
 
   return (
     <>
       <Title className={styles.title} level={2}>
         {title}
       </Title>
+
+      <div ref={carouselRef}>
+        <AntdCarousel infinite variableWidth>
+          {[].constructor
+            .apply({}, new Array(20))
+            .map((_: unknown, index: number) => (
+              <div key={index}>
+                <div
+                  style={{
+                    width: imageSize,
+                    height: imageSize,
+                  }}
+                >
+                  {index}
+                </div>
+              </div>
+            ))}
+        </AntdCarousel>
+      </div>
 
       <div className={styles.more}>
         <Button type="primary" ghost>
