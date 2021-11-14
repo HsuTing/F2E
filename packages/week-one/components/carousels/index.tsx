64d979811bf0ccade459678c55b50cc505bc4a82
HsuTing <hsuting0106@gmail.com>
2021-11-14 15:10:04 +0800

    feat(week-one): use carousel fragment

--- packages/week-one/components/carousels/index.tsx
+++ packages/week-one/components/carousels/index.tsx
@@ -1,57 +1,72 @@
 import React from 'react';
 import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
+import { filter } from 'graphql-anywhere';
 import { Divider } from 'antd';
 
 import type { carouselsFragment as carouselsFragmentType } from '../../gqls';
 
 import Carousel from './Carousel';
+import { carouselFragment } from './gqls/carousel';
 import styles from './styles/index.module.scss';
 
-const Carousels = (_: Partial<carouselsFragmentType>) => {
+const Carousels = ({
+  scenicSpots,
+  hotels,
+  activities,
+}: Partial<carouselsFragmentType>) => {
   const { t } = useTranslation('carousels');
 
   return (
     <div className={styles.root}>
-      <Carousel
-        title={
-          <>
-            <span>
-              <Image src="/fire.svg" width={32} height={32} />
-            </span>
+      {!scenicSpots ? null : (
+        <Carousel
+          title={
+            <>
+              <span>
+                <Image src="/fire.svg" width={32} height={32} />
+              </span>
 
-            {t('scenic-spots')}
-          </>
-        }
-      />
-
-      <Divider />
-
-      <Carousel
-        title={
-          <>
-            <span>
-              <Image src="/company.svg" width={32} height={32} />
-            </span>
-
-            {t('hotels')}
-          </>
-        }
-      />
+              {t('scenic-spots')}
+            </>
+          }
+          data={filter(carouselFragment, scenicSpots)}
+        />
+      )}
 
       <Divider />
 
-      <Carousel
-        title={
-          <>
-            <span>
-              <Image src="/calendar.svg" width={32} height={32} />
-            </span>
+      {!hotels ? null : (
+        <Carousel
+          title={
+            <>
+              <span>
+                <Image src="/company.svg" width={32} height={32} />
+              </span>
 
-            {t('activities')}
-          </>
-        }
-      />
+              {t('hotels')}
+            </>
+          }
+          data={filter(carouselFragment, hotels)}
+        />
+      )}
+
+      <Divider />
+
+      {!activities ? null : (
+        <Carousel
+          title={
+            <>
+              <span>
+                <Image src="/calendar.svg" width={32} height={32} />
+              </span>
+
+              {t('activities')}
+            </>
+          }
+          data={filter(carouselFragment, activities)}
+        />
+      )}
     </div>
   );
 };
