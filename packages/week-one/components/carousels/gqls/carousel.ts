d043391e90aefb2c8796da8dcd0208dd69f9efd5
HsuTing <hsuting0106@gmail.com>
2021-11-14 15:23:01 +0800

    fix(week-one): fix types

--- packages/week-one/components/carousels/gqls/carousel.ts
+++ packages/week-one/components/carousels/gqls/carousel.ts
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client';
 export const carouselFragment = gql`
   fragment carouselFragment on Info {
     id: ID
-    picture: Picture {
+    picture: Picture @type(name: "Picture") {
       url: PictureUrl1
     }
   }
--- packages/week-one/components/carousels/gqls/index.ts
+++ packages/week-one/components/carousels/gqls/index.ts
@@ -6,7 +6,7 @@ export const carouselsFragment = gql`
   fragment carouselsFragment on Query {
     scenicSpots(first: 15)
       @rest(
-        type: "[ScenicSpot!]!"
+        type: "[ScenicSpot]"
         path: "/Tourism/ScenicSpot?$top={args.first}&$format=JSON"
       ) {
       ...carouselFragment
@@ -15,7 +15,7 @@ export const carouselsFragment = gql`
 
     hotels(first: 15)
       @rest(
-        type: "[Hotel!]!"
+        type: "[Hotel]"
         path: "/Tourism/Hotel?$top={args.first}&$format=JSON"
       ) {
       ...carouselFragment
@@ -24,7 +24,7 @@ export const carouselsFragment = gql`
 
     activities(first: 15)
       @rest(
-        type: "[Activity!]!"
+        type: "[Activity]"
         path: "/Tourism/Activity?$top={args.first}&$format=JSON"
       ) {
       ...carouselFragment
