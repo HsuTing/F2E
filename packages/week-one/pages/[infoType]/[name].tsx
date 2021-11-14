58045d74680ae2183cea121887067d8c77885e63
HsuTing <hsuting0106@gmail.com>
2021-11-14 23:21:06 +0800

    feat(week-one): add detail page query

--- packages/week-one/pages/[infoType]/[name].tsx
+++ packages/week-one/pages/[infoType]/[name].tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useQuery } from '@apollo/client';
+import { filter } from 'graphql-anywhere';
 
+import Carousels from '../../components/carousels';
+import type { getDetailPage as getDetailPageType } from '../../gqls/types';
+import { getDetailPage } from '../../gqls/detail';
+import { carouselsFragment } from '../../components/carousels/gqls';
+import { initializeApollo } from '../../hooks/useApollo';
 import { INFO_TYPES } from '../../utils/constants';
 
 const Detail = () => {
-  return null;
+  const { data } = useQuery<getDetailPageType>(getDetailPage);
+
+  return <Carousels {...filter(carouselsFragment, data || {})} />;
 };
 
 export const getServerSideProps = async ({
@@ -19,9 +28,20 @@ export const getServerSideProps = async ({
 }) => {
   if (!INFO_TYPES.includes(infoType)) return { notFound: true };
 
+  const client = initializeApollo();
+
+  try {
+    await client.query<getDetailPageType>({
+      query: getDetailPage,
+    });
+  } catch (e) {
+    // error would be handled in useApollo
+  }
+
   return {
     props: {
       ...(await serverSideTranslations(locale, ['common'])),
+      initialApolloState: client.cache.extract(),
       name,
     },
   };
