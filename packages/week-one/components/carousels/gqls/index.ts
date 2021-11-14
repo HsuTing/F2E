5d5c90c664f3a2841dde7bc1897948c0af4c8083
HsuTing <hsuting0106@gmail.com>
2021-11-15 01:45:32 +0800

    feat(week-one): add info query

--- packages/week-one/components/carousels/gqls/index.ts
+++ packages/week-one/components/carousels/gqls/index.ts
@@ -4,28 +4,28 @@ import { carouselFragment } from './carousel';
 
 export const carouselsFragment = gql`
   fragment carouselsFragment on Query {
-    scenicSpots: infos(type: ScenicSpot, first: 15)
+    scenicSpots: infos(infoType: ScenicSpot, first: 15)
       @rest(
         type: "[Info]"
-        path: "/Tourism/{args.type}?$top={args.first}&$format=JSON"
+        path: "/Tourism/{args.infoType}?$top={args.first}&$format=JSON"
       ) {
       ...carouselFragment
       id: ID
     }
 
-    hotels: infos(type: Hotel, first: 15)
+    hotels: infos(infoType: Hotel, first: 15)
       @rest(
         type: "[Info]"
-        path: "/Tourism/{args.type}?$top={args.first}&$format=JSON"
+        path: "/Tourism/{args.infoType}?$top={args.first}&$format=JSON"
       ) {
       ...carouselFragment
       id: ID
     }
 
-    activities: infos(type: Activity, first: 15)
+    activities: infos(infoType: Activity, first: 15)
       @rest(
         type: "[Info]"
-        path: "/Tourism/{args.type}?$top={args.first}&$format=JSON"
+        path: "/Tourism/{args.infoType}?$top={args.first}&$format=JSON"
       ) {
       ...carouselFragment
       id: ID
--- packages/week-one/gqls/detail.ts
+++ packages/week-one/gqls/detail.ts
@@ -3,8 +3,15 @@ import { gql } from '@apollo/client';
 import { carouselsFragment } from '../components/carousels/gqls';
 
 export const getDetailPage = gql`
-  query getDetailPage {
+  query getDetailPage($id: ID!, $infoType: InfoTypeEnum!) {
     ...carouselsFragment
+    info(id: $id, infoType: $infoType)
+      @rest(
+        type: "[Info]"
+        path: "/Tourism/{args.infoType}?$top=1&$filter=id eq {args.id}&$format=JSON"
+      ) {
+      id: ID
+    }
   }
 
   ${carouselsFragment}
