8a4a681a0ea4daeaa28ccab3c282937b6549c64b
HsuTing <hsuting0106@gmail.com>
2021-11-14 19:16:15 +0800

    fix(week-one): fix arrow style

--- packages/week-one/components/carousels/Arrow.tsx
+++ packages/week-one/components/carousels/Arrow.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Button } from 'antd';
 
+import styles from './styles/arrow.module.scss';
+
 interface PropsType {
+  className?: string;
   onClick?: () => void;
   icon: React.ReactElement;
 }
 
-const Arrow = ({ onClick, icon }: PropsType) => (
-  <Button onClick={onClick} icon={icon} shape="circle" />
+const Arrow = ({ className, onClick, icon }: PropsType) => (
+  <Button
+    className={`${styles.root} ${
+      /next/.test(className || '') ? styles.next : styles.prev
+    }`}
+    onClick={onClick}
+    icon={icon}
+    shape="circle"
+    size="large"
+  />
 );
 
 export default React.memo(Arrow);
