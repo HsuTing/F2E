14406c1c8d5ca375449af941e2336b9132f7fa7f
HsuTing <hsuting0106@gmail.com>
2022-02-01 13:53:38 +0800

    feat(week-one): add heart button

--- packages/week-one/components/Info.tsx
+++ packages/week-one/components/Info.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
-import { Typography, Space } from 'antd';
+import { Typography, Space, Button } from 'antd';
+import { HeartOutlined } from '@ant-design/icons';
 
 import styles from './styles/info.module.scss';
 import type { infoFragment as infoFragmentType } from '../gqls/types';
@@ -13,7 +14,12 @@ const { Title, Text } = Typography;
 
 const Info = ({ info: { name, address, phone, date } }: PropsType) => (
   <>
-    <Title className={styles.title}>{name}</Title>
+    <Title className={styles.title}>
+      {name}
+
+      {/* TODO */}
+      <Button icon={<HeartOutlined />} shape="circle" size="large" />
+    </Title>
 
     {/* TODO */}
     <Text className={styles.rating}>4.1 古色古香，適合攜家帶眷！</Text>
