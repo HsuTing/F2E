f3c5b8f3749ea1b0a9b28ec8ab1134cbe88b84b1
HsuTing <hsuting0106@gmail.com>
2022-02-01 10:11:44 +0800

    feat(week-one): add some info

--- packages/week-one/components/Info.tsx
+++ packages/week-one/components/Info.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Typography, Space } from 'antd';
 
 import type { infoFragment as infoFragmentType } from '../gqls/types';
 
@@ -6,8 +7,32 @@ interface PropsType {
   info: infoFragmentType;
 }
 
-const Info = (_: PropsType) => {
-  return null;
-};
+const { Title, Text } = Typography;
+
+const Info = ({ info: { name, address, phone, date } }: PropsType) => (
+  <>
+    <Title>{name}</Title>
+
+    <Space direction="vertical">
+      {[
+        {
+          key: 'address',
+          children: address,
+          underline: true,
+        },
+        {
+          key: 'phone',
+          children: phone,
+        },
+        {
+          key: 'date',
+          children: date,
+        },
+      ].map(({ key, ...props }) => (
+        <Text {...props} key={key} />
+      ))}
+    </Space>
+  </>
+);
 
 export default React.memo(Info);
