6124e6c39fe0b529aa002a0939e479dc0fa9d079
HsuTing <hsuting0106@gmail.com>
2022-02-20 16:17:06 +0800

    feat(week-one): add info layout

--- packages/week-one/components/info/index.tsx
+++ packages/week-one/components/info/index.tsx
@@ -30,99 +30,103 @@ const Info = ({
 
   return (
     <div className={styles.root}>
-      <Breadcrumb>
-        {[
-          {
-            key: 'taiwan',
-            href: '/',
-          },
-          {
-            key: `cities.${city}`,
-            href: `/${city}`,
-          },
-          {
-            key: infoType,
-            href: `/${city}/${infoType}`,
-          },
-          {
-            key: name,
-          },
-        ].map(({ key, href }: { key: string; href?: string }) => (
-          <Item key={key}>
-            {!href ? (
-              key
-            ) : (
-              <Link href={href}>
-                <a>{t(`common:${key}`)}</a>
-              </Link>
-            )}
-          </Item>
-        ))}
-      </Breadcrumb>
+      <div>
+        <Breadcrumb>
+          {[
+            {
+              key: 'taiwan',
+              href: '/',
+            },
+            {
+              key: `cities.${city}`,
+              href: `/${city}`,
+            },
+            {
+              key: infoType,
+              href: `/${city}/${infoType}`,
+            },
+            {
+              key: name,
+            },
+          ].map(({ key, href }: { key: string; href?: string }) => (
+            <Item key={key}>
+              {!href ? (
+                key
+              ) : (
+                <Link href={href}>
+                  <a>{t(`common:${key}`)}</a>
+                </Link>
+              )}
+            </Item>
+          ))}
+        </Breadcrumb>
 
-      <Title className={styles.title}>
-        {name}
+        <Title className={styles.title}>
+          {name}
+
+          {/* TODO */}
+          <Button icon={<HeartOutlined />} shape="circle" size="large" />
+        </Title>
 
         {/* TODO */}
-        <Button icon={<HeartOutlined />} shape="circle" size="large" />
-      </Title>
+        <Text className={styles.rating}>4.1 古色古香，適合攜家帶眷！</Text>
 
-      {/* TODO */}
-      <Text className={styles.rating}>4.1 古色古香，適合攜家帶眷！</Text>
+        <Space className={styles.info} direction="vertical">
+          {(
+            [
+              {
+                key: 'address',
+                underline: true,
+              },
+              {
+                key: 'phone',
+              },
+              {
+                key: 'date',
+              },
+            ] as const
+          ).map(({ key, ...props }) => (
+            <Text {...props} key={key}>
+              <>
+                <span>
+                  <Image src={`/info/${key}.svg`} width={20} height={20} />
+                </span>
 
-      <Space className={styles.info} direction="vertical">
-        {(
-          [
-            {
-              key: 'address',
-              underline: true,
-            },
-            {
-              key: 'phone',
-            },
-            {
-              key: 'date',
-            },
-          ] as const
-        ).map(({ key, ...props }) => (
-          <Text {...props} key={key}>
-            <>
-              <span>
-                <Image src={`/info/${key}.svg`} width={20} height={20} />
-              </span>
+                {info[key]}
+              </>
+            </Text>
+          ))}
+        </Space>
 
+        <Tabs className={styles.tabs}>
+          {(
+            [
+              'descriptionDetail',
+              'ticketInfo',
+              'travelInfo',
+              'remarks',
+              'comments',
+            ] as const
+          ).map(key => (
+            <TabPane key={key} tab={t(key)}>
               {info[key]}
-            </>
-          </Text>
-        ))}
-      </Space>
+            </TabPane>
+          ))}
+        </Tabs>
 
-      <Tabs className={styles.tabs}>
-        {(
-          [
-            'descriptionDetail',
-            'ticketInfo',
-            'travelInfo',
-            'remarks',
-            'comments',
-          ] as const
-        ).map(key => (
-          <TabPane key={key} tab={t(key)}>
-            {info[key]}
-          </TabPane>
-        ))}
-      </Tabs>
+        <Button
+          className={styles.goButton}
+          href={websiteUrl}
+          type="primary"
+          ghost
+        >
+          {t('go-to-website')}
+        </Button>
+      </div>
 
-      <Button
-        className={styles.goButton}
-        href={websiteUrl}
-        type="primary"
-        ghost
-      >
-        {t('go-to-website')}
-      </Button>
-
-      <InfoCarousel pictures={filter(infoCarouselFragment, pictures)} />
+      <div>
+        <InfoCarousel pictures={filter(infoCarouselFragment, pictures)} />
+      </div>
     </div>
   );
 };
