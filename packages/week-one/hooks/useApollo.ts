95b8c693b358964ca1fe2b58ea42e51195b501c2
HsuTing <hsuting0106@gmail.com>
2022-02-20 14:40:03 +0800

    fix(week-one): fix empty pictures

--- packages/week-one/hooks/useApollo.ts
+++ packages/week-one/hooks/useApollo.ts
@@ -48,8 +48,11 @@ const format = (data: formatDataType): formatDataType => {
       : Object.entries(data).reduce(
           (result, [key, value]) => ({
             ...result,
-            [KEYS[key] || lowerFirst(key)]:
-              isEmpty(value) && key !== 'Picture' ? null : format(value),
+            [KEYS[key] || lowerFirst(key)]: (() => {
+              if (isEmpty(value)) return key !== 'Picture' ? null : [];
+
+              return format(value);
+            })(),
           }),
           {},
         );
